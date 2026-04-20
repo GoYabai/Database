39a48d68d018f32ab940d2f488e8faa42c959622
GoYabai <leduchuyqazzaq@gmail.com>
2026-04-20 21:25:31 +0700

    Day 4

---
+++ Calculate_Special_Bonus/solve.sql
@@ -0,0 +1,16 @@
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    name VARCHAR(255),
+    salary INT
+);
+
+SELECT
+    employee_id,
+    CASE
+        WHEN employee_id % 2 != 0 AND name NOT LIKE 'M%' THEN salary
+        ELSE 0
+    END AS bonus
+FROM employees
+ORDER BY employee_id;
+
+
