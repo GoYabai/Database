2c5d47e88fcdfa7f75a291e879146b251f32fb46
GoYabai <leduchuyqazzaq@gmail.com>
2026-04-22 14:11:43 +0700

    Day 6

---
+++ Duplicate_email/solve.sql
@@ -0,0 +1,18 @@
+DROP TABLE Person;
+
+
+CREATE TABLE Person (
+    id INT PRIMARY KEY,
+    email VARCHAR(255)
+);
+
+COPY Person(id, email)
+FROM 'C:\\Project\\Database\\Duplicate_email\\Person.csv'
+DELIMITER ','
+CSV HEADER;
+
+
+SELECT email
+FROM Person
+GROUP BY email
+HAVING count(*) > 1;
